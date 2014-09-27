d2b8e874baec019e9f2d654b15ac62a01fc26e68
dan <rehman.d.fazlur@gmail.com>
2014-09-26 22:03:14 -0700

    Adding arp git commit -m Adding

---
+++ test/arp/create_arp.c
@@ -0,0 +1,32 @@
+
+// Define a struct for ARP header
+typedef struct _arp_hdr arp_hdr;
+struct _arp_hdr {
+  uint16_t htype;
+  uint16_t ptype;
+  uint8_t hlen;
+  uint8_t plen;
+  uint16_t opcode;
+  uint8_t sender_mac[6];
+  uint8_t sender_ip[4];
+  uint8_t target_mac[6];
+  uint8_t target_ip[4];
+};
+
+// Define some constants.
+#define ETH_HDRLEN 14      // Ethernet header length
+#define IP4_HDRLEN 20      // IPv4 header length
+#define ARP_HDRLEN 28      // ARP header length
+#define ARPOP_REQUEST 1    // Taken from <linux/if_arp.h>
+
+int main(int argc, char *argv[]){
+
+    if ((sd = socket (AF_INET, SOCK_RAW, IPPROTO_RAW)) < 0) {
+       perror ("socket() failed to get socket descriptor for using ioctl() ");
+      exit (EXIT_FAILURE);
+    }
+
+
+
+}
+
